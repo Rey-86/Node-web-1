55c524d933049faedc5cea6ea304e27d6fe4879a
Rey-86 <reygranhacristian@gmail.com>
2023-01-30 18:16:29 +0100

    petición post e instalación de body-parse

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const path = require('path');
+var bodyparser=require('body-parser')
 const app = express()
 const port = 3000
+
+app.use(bodyparser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
 let options = {
         root:path.join(__dirname)
 }
-       
-
     res.sendFile("/views/index.html", options);
 })
-
+app.post('/', (req, res) => {
+    console.log(req);
+    res.send('recibiendo datos')
+    
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
